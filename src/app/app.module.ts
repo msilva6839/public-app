1437e6bbd8751789e1fadf360ef4bac317490a1f
msilva6839 <msilva6839@utm.edu.ec>
2024-07-31 12:43:35 -0500

    Cambios relacionados al desarrollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Asegúrate de que FormsModule esté importado
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
